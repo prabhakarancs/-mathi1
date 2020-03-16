150735190be3f64b1bdc64056652eab13741d587
prabhakarancs <61134162+prabhakarancs@users.noreply.github.com>
2020-03-16 11:54:30 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,52 @@
+class  pa
+  
+ {
+      
+int j,i,a=5;
+  
+     void display()
+     
+  {
+       
+    
+           for(i=0;i<a;i++)
+    
+       {
+          
+     for(j=0;j<=i;j++)
+        
+       {
+    
+               System.out.print("* ");
+ 
+              }
+       
+        System.out.println();
+        
+   }
+      
+ }
+  
+ }
+
+
+ 
+class  Main
+
+{
+
+public static void main(String arg[])
+
+{
+ 
+   pa p=new pa();
+ 
+   p.display();
+   
+ 
+}
+
+}
+   
+
+
